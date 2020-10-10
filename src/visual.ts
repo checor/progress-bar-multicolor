6d55981a97c72e29d2d80c90fc71514b6f24de2b
Sergio Urbina <checor@gmail.com>
2020-10-10 15:01:14 -0500

    Changed order, updated input to build

--- src/visual.ts
+++ src/visual.ts
@@ -36,17 +36,13 @@ export class Visual implements IVisual {
         if(options.dataViews && options.dataViews[0]){
             const dataView: DataView = options.dataViews[0];
             const data: any = dataView.table.rows;
-            if (!data || data.length == 0){
+            if (!data || data.length < 3){
                 this.clear();
                 return;
             }
-            if (data[0].length == 3){
-                console.log(typeof data[0][2]);
-                data.sort((a, b) => new Date(a[2]).getTime() - new Date(b[2]).getTime());
-            }
             ReactProgressBar.update({
-                widths: data.map(x => x[0]),
-                colors: data.map(x => x[1]),
+                widths: data.map(x => x[1]),
+                colors: data.map(x => x[2]),
             });
         } else {
             this.clear();
